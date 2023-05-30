351622bfddfe44cf56c577bd919958439c2bd2c8
JoseParada19 <131200876+JoseParada19@users.noreply.github.com>
2023-05-30 16:23:31 -0400

    objeto nombre contraseña
    nombre, contraseña ingreasado en aplicacion

--- js3.js
+++ js3.js
@@ -3,9 +3,9 @@ function formulario(){
     let name= eName.value;
     console.log(name)
 
-    let eContra= document.getElementById("contraseñaa")
-    let contraseña= eContra.value;
-    console.log(contraseña)
+    let eContra= document.getElementById("contraseniaa")
+    let contrasenia= eContra.value;
+    console.log(contrasenia)
 
     let eHombre= document.getElementById("hombre")
     let hombre= eHombre.Checked;
@@ -18,51 +18,38 @@ function formulario(){
     let eOtro= document.getElementById("otro")
     let otro= eOtro.Checked;
     console.log(otro)
-
-    let eEdad1= document.getElementById("edad1")
-    let edad1= eEdad1.value;
-    console.log(edad1)
-
-    let eEdad2= document.getElementById("edad2")
-    let edad2= eEdad2.value;
-    console.log(edad2)
-
-    let eEdad3= document.getElementById("edad3")
-    let edad3= eEdad3.value;
-    console.log(edad3)
-
-    let eEdad4= document.getElementById("edad4")
-    let edad4= eEdad4.value;
-    console.log(edad4)
-
-    let eEdad5= document.getElementById("edad5")
-    let edad5= eEdad5.value;
-    console.log(edad5)
-
-    let eEdad6= document.getElementById("edad6")
-    let edad6= eEdad6.value;
-    console.log(edad6)
-
-    let eEdad7= document.getElementById("edad7")
-    let edad7= eEdad7.value;
-    console.log(edad7)
-
-    let eEdad8= document.getElementById("edad8")
-    let edad8= eEdad8.value;
-    console.log(edad8)
-
-    let eEdad9= document.getElementById("edad9")
-    let edad9= eEdad9.value;
-    console.log(edad9)
-
-
-
-
-
-
-
-
-
-
 }
 document.getElementById("enviar").addEventListener("click",formulario)
+
+
+
+
+
+var registro=()=>{
+    let enombre = document.getElementById("nombreapellido");
+    let econtraseña = document.getElementById("contraseniaa");
+    let nombree = enombre.value;
+    let contraseniaaa = econtraseña.value;
+
+    console.log(nombree)
+    console.log(contraseniaaa)
+    
+    let persona ={"nombreapellido":nombree,"contraseniaaa":contraseniaaa}
+    let listadopersonas=localStorage.getItem("personas");
+    let listadoantiguo=JSON.parse(listadopersonas);
+
+
+
+    if(listadoantiguo==null){
+        listadonuevo =[persona]
+    }else{
+        listadoantiguo.push(persona)
+        listadonuevo =[...listadoantiguo]
+    }
+    
+    console.log(persona)
+    console.log(listadoantiguo)
+    console.log(listadonuevo)
+    localStorage.setItem("personas",JSON.stringify(listadonuevo));
+}
+    document.getElementById("enviar").addEventListener("click", registro)
